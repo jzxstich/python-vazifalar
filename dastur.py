d25b2ae1b7df8b1f02c5a4aa1a39413778e503a5
jzxstich <jzxstich@gmail.com>
2025-02-04 01:16:36 +0500

    Create dastur.py

---
+++ dastur.py
@@ -0,0 +1,9 @@
+
+users = ['alisher1983','aziza','yasina' 'umar']
+
+login = input('Yangi login tanlang:' )
+
+if login.lower() in users:
+    print('Login band, yangi login tanalng!')
+else:
+    print("Xush kelibsiz!")
